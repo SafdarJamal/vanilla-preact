db877015826aec8f6cf7ed327c68c60dc4ad87bc
Julian Böhnke <info@julianb.de>
2020-04-04 23:28:52 +0200

    add the newest Version from preact and htm

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import {
   h,
   render
-} from 'https://unpkg.com/preact@10.0.0/dist/preact.module.js';
-import htm from 'https://unpkg.com/htm@2.2.1/dist/htm.module.js';
+} from 'https://unpkg.com/preact@10.3.4/dist/preact.module.js';
+import htm from 'https://unpkg.com/htm@3.0.3/dist/htm.module.js';
 window.html = htm.bind(h);
 
 import App from './components/App.js';
